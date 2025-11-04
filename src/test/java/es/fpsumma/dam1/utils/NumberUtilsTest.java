e0d66852d7a191d523600e0e22f8f6fa71ebb3eb
frc16 <153406170+frc16@users.noreply.github.com>
2025-11-04 12:35:57 +0100

    1

--- src/test/java/es/fpsumma/dam1/utils/NumberUtilsTest.java
+++ src/test/java/es/fpsumma/dam1/utils/NumberUtilsTest.java
@@ -1,10 +1,37 @@
 package es.fpsumma.dam1.utils;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class NumberUtilsTest {
-
+    @Test
+    public void sumaOK(){
+        int resultado = NumberUtils.suma(2, 03);
+        assertEquals(5, resultado);
+    }
    
+    @test 
+    public void restaOK(){
+        int resultado = NumberUtils.subtract(10, 5);
+        assertEquals(2, resultado);
+    }
+
+    @test
+
+    public void multiplyOK(){
+        int resultado = NumberUtils.multiply(3, 3);
+        assertEquals(9, resultado);
+    }
+
+    @test 
+
+    public void isEvenOK(){
+        boolean resultado = NumberUtils.isEven(10);
+        assertEquals(true,resultado);
+    }
+
+    @test
+
+    public void 
 }
---
+++ src/test/java/es/fpsumma/dam1/utils/test.java
@@ -0,0 +1,5 @@
+package es.fpsumma.dam1.utils;
+
+public @interface test {
+
+}
